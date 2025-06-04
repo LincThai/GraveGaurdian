8e24ac73d6436a8e2d4c8f5615652e25bb660fb7
LincThai <78945845+LincThai@users.noreply.github.com>
2025-06-04 20:08:19 +1000

    Setup Settings, PlayerPrefs and Builds

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,11 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        // unlock cursor when in the main menu scene
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // starts the game
     public void StartButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // quits the game leaving the closing the application
     public void QuitButton()
     {
         Application.Quit();
--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,9 +1,30 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+    // set variables
+    // references
+    public AudioMixer audioMixer;
+
+    // adjust Master audiomixer group by main volume slider
     public void SetMainVolume(float volume)
     {
-        Debug.Log(volume);
+        audioMixer.SetFloat("MainVolume", volume);
+        PlayerPrefs.SetFloat("mainVolume", volume);
+    }
+
+    // adjust BGM audiomixer group by bgm volume slider
+    public void SetBGMVolume(float volume)
+    {
+        audioMixer.SetFloat("BGMVolume", volume);
+        PlayerPrefs.SetFloat("bgmVolume", volume);
+    }
+
+    // adjust SFX audiomixer group by sfx volume slider
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
     }
 }
